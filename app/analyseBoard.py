ceaf617943bd689fc5d1cfd0298c4b99dafd9343
Spt <intspt@qq.com>
2015-05-23 16:05:59 +0800

    Update analyseBoard.py

--- app/analyseBoard.py
+++ app/analyseBoard.py
@@ -20,12 +20,13 @@ def analyseBoard():
             team_name = tr.find_all('td')[1].get_text().split('\n')[0]
             problem_status_list = tr.find_all('td')[4: 4 + PROBLEM_NUM]
             team = Team.query.filter_by(name=team_name).first()
+            team_location, team_problem_solved = team.location, team.problem_solved
             for (i, problem_status) in enumerate(problem_status_list):
                 if '/' in problem_status.get_text():
-                    if ((team.problem_solved >> i) & 1) == 0:
+                    if ((team_problem_solved >> i) & 1) == 0:
                         problem_solved = Team.query.filter_by(name=team_name).first().problem_solved
                         Team.query.filter_by(name=team_name).update({'problem_solved': problem_solved | (1 << i)})
-                        send_balloon = SendBalloon(team.name, i, team.location)
+                        send_balloon = SendBalloon(team_name, i, team_location)
                         db.session.add(send_balloon)
                         db.session.commit()
                         db.session.close()
